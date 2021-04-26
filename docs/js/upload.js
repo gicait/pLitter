1b7c0e196cf2e37b9dad545cdccc1e66c6b64572
sriramreddyM <srirammandhati@gmail.com>
2021-04-27 00:30:28 +0700

    image onload model run

--- docs/js/upload.js
+++ docs/js/upload.js
@@ -149,8 +149,8 @@ function open_image(image_id){
     // .then(data => console.log(data))
     // .catch(error => console.log(error))
     // $( "image-veiwer" ).remove();
-    
-    document.getElementById("image-veiwer").innerHTML = "<div id='save'></div><img id='ann-img' crossorigin='anonymous' src='http://203.159.29.187:8080/api/image/"+image_id+"' width=100% height=auto onload='predict_on_this()'/>"
+    // width=100% height=auto 
+    document.getElementById("image-veiwer").innerHTML = "<div id='save'></div><img id='ann-img' crossorigin='anonymous' src='http://203.159.29.187:8080/api/image/"+image_id+"'  onload='predict_on_this()'/>"
     // document.getElementById("image-veiwer").innerHTML("<img src='http://203.159.29.187:8080/api/image/"+image_id.toString()+"' />" )
 
 
