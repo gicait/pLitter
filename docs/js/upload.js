18a516c6076845af3ad4c54ad5957cc74ba262ef
sriramreddyM <srirammandhati@gmail.com>
2021-04-28 19:43:31 +0700

    upload & annotate seperate

--- docs/js/upload.js
+++ docs/js/upload.js
@@ -4,8 +4,9 @@ var set_user = false
 var images = {}
 var anno;
 var new_image_id;
-
-
+var stats = {}
+var dataset = {}
+var annotator_response = {}
 
 function drawBoxes_annotorius(image, predictions){
     // console.log(predictions)
@@ -99,6 +100,133 @@ var cat_dict = {
   };
 
 
+function get_annots_from_coco(im_id){
+    alert("yeee")
+    fetch("http://203.159.29.187:8080/api/annotator/data/"+String(im_id), {
+        "headers": {
+            "accept": "application/json",
+            "accept-language": "en-US,en;q=0.9"
+        },
+        "referrer": "http://203.159.29.187:8080/api/",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    })
+    .then(response => response.json())
+    .then(data => annotator_response = data)
+    .then(() => {
+        for(cat_num in annotator_response.categories){
+            var category = annotator_response.categories[cat_num]
+            var cat_name = category["name"]
+            console.log(cat_name)
+            var annotations = category["annotations"]
+            var annotations_format = []
+
+
+            // to do from below
+
+            // *********************
+            // *************
+
+
+    //         annotations.forEach(annotation => {
+    //             const {box, label, score} = prediction;
+    //             const {left, top, width, height} = box;
+    //     var pred_dict = {}
+    //     pred_dict["type"] = "Annotation"
+    //     pred_dict["body"] = []
+    //     pred_dict_body = {}
+    //     pred_dict_body["type"] =  "TextualBody"
+    //     pred_dict_body["purpose"] = "tagging"
+    //     pred_dict_body["value"] = "Plastic"
+    //     pred_dict["body"].push(pred_dict_body)
+        
+    //     pred_dict["target"] = {}
+    //     pred_dict["target"]["source"] = "http://203.159.29.187:8080/api/image/28568",
+    //     pred_dict["target"]["selector"] = {}
+    //     pred_dict["target"]["selector"]["type"] = "FragmentSelector"
+    //     pred_dict["target"]["selector"]["conformsTo"] = "http://www.w3.org/TR/media-frags/"
+
+    //     // convet to natual dimentions to img align dimensions
+
+    //     a_left = left * (image.naturalWidth/image.width)
+    //     a_top = top * (image.naturalHeight/image.height)
+    //     a_width = width * (image.naturalWidth/image.width)
+    //     a_height = height * (image.naturalHeight/image.height)
+        
+    //     pred_dict["target"]["selector"]["value"] = "xywh=pixel:"+String(a_left)+","+String(a_top)+","+String(a_width)+","+String(a_height)
+        
+    //     pred_dict["@context"] = ""
+    //     pred_dict["id"] = ""
+
+    //     preds_format.push(pred_dict)
+    // });
+    // console.log(preds_format)
+
+    // if annotation is ready, first save it to backend and change with response id
+
+
+
+    anno.setAnnotations(preds_format)
+
+
+
+
+        }
+    })
+    .catch(error => console.log(error))
+}
+
+
+function save_annots_to_coco(im_id){
+    var anns = ran_anno.getAnnotations()
+    alert("Confirm saving"+anns.length+"annotations")
+    console.log(anns)
+    var ann_id;
+    var ann;
+    for(ann_id in anns){
+        ann = anns[ann_id]
+            if(ann.target.selector.type === 'FragmentSelector' & ann.target.selector.conformsTo === "http://www.w3.org/TR/media-frags/"){
+            var value = ann.target.selector.value    
+            var format = value.includes(':') ? value.substring(value.indexOf('=') + 1, value.indexOf(':')) : 'pixel';
+            var coords = value.includes(':') ? value.substring(value.indexOf(':') + 1) : value.substring(value.indexOf('=') + 1); 
+            var [ x, y, w, h ] = coords.split(',').map(parseFloat)
+            var cat_name = ann.body[0].value
+            var cat_id = cat_dict[cat_name]
+            console.log(x, y, w, h, cat_id, ann.id)
+
+
+            var seg = [[x,y,x+w,y,x+w,y+h,x,y+h]]
+            var annot_metadata = {'predicted':true}
+            
+            fetch("http://203.159.29.187:8080/api/annotation/", {
+                "headers": {
+                  "accept": "application/json, text/plain, */*",
+                  "accept-language": "en-US,en;q=0.9",
+                  "content-type": "application/json;charset=UTF-8"
+                },
+                "referrer": "http://203.159.29.187:8080/",
+                "referrerPolicy": "strict-origin-when-cross-origin",
+                "body": JSON.stringify({
+                    image_id: im_id,
+                    category_id: cat_id,
+                    isbbox: true,
+                    segmentation: seg,
+                    metadata: annot_metadata
+                }),
+                "method": "POST",
+                "mode": "cors",
+                "credentials": "include"
+              });
+        }
+    }
+    im_status = im_id+"_status"
+}
+
+
+
 function save_anntations_in_coco(im_id){
     var anns = anno.getAnnotations()
     alert("confirm saving"+anns.length+"annotations")
@@ -117,7 +245,7 @@ function save_anntations_in_coco(im_id){
             console.log(x, y, w, h, cat_id)
 
             var seg = [[x,y,x+w,y,x+w,y+h,x,y+h]]
-            var annot_metadata = JSON.stringify({"predicted":true})
+            var annot_metadata = {'predicted':true}
             // annot_metadata["predicted"] = true
             // [[140,273,170,273,170,280,140,280]]
             // "{\"image_id\":"+image_id+",\"category_id\":"+cat_id+",\"isbbox\":true,\"segmentation\":"+segmentation+"}",
@@ -146,6 +274,78 @@ function save_anntations_in_coco(im_id){
     im_status = im_id+"_status"
     document.getElementById(im_status).innerHTML="true" 
 }
+
+
+function load_random(){
+    fetch("http://203.159.29.187:8080/api/dataset/55/data?page=1&limit=50&annotated=false", {
+        "headers": {
+            "accept": "application/json",
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
+        },
+        "referrer": "http://203.159.29.187:8080/",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    })
+    .then(response => response.json())
+    .then(data => images = data)
+    .then(() => {
+        const random_index = Math.floor(Math.random() * images.images.length);
+        const image = images.images[random_index];
+        const image_id = image["id"]
+        document.getElementById("ran-img").innerHTML = "<div id='save'></div><img id='ran-ann-img' crossorigin='anonymous' src='http://203.159.29.187:8080/api/image/"+image_id+"' width=100% height=auto/>"
+
+        get_annots_from_coco(image_id)
+
+        ran_anno = Annotorious.init({
+            image: 'ran-ann-img',
+            locale: 'auto',
+            // disableEditor: true
+            widgets: [
+                // { widget: 'COMMENT' },
+                // { widget: return    },
+                { widget: 'TAG', vocabulary: [ 'Plastic', 'Pile', 'Trash Bin', 'Face mask'] }
+            ]
+        })
+        
+        // model_run()
+        document.getElementById("save").innerHTML = "<button onclick='save_annots_to_coco("+image_id+")'>Save</button>"
+        
+        ran_anno.on('createSelection', async function(selection) {
+            selection.body = [{
+            type: 'TextualBody',
+            purpose: 'tagging',
+            value: 'Plastic'
+
+            }];
+            // selection.id = "#0000"
+            console.log(selection)
+            await ran_anno.updateSelected(selection);
+            ran_anno.saveSelected();
+        })
+        
+        ran_anno.on('selectAnnotation', function(a) {
+            console.log('selectAnnotation', a);
+        })
+        
+        ran_anno.on('cancelSelected', function(a) {
+            console.log('cancelSelected', a);
+        })
+        
+        ran_anno.on('createAnnotation', function(a) {
+            console.log('created', a);
+        })
+        
+        ran_anno.on('updateAnnotation', function(annotation, previous) {
+            console.log('updated', previous, 'with', annotation);
+        })
+    })
+    .catch(error => console.log(error))
+}
+
+
 
 function open_image(image_id){
     // fetch("http://203.159.29.187:8080/api/annotator/data/"+image_id.toString(), {
@@ -362,6 +562,35 @@ function add_with_id(res_id, im_name){
     $(imagelist).find('tbody').prepend("<tr id="+res_id+" onclick='open_image("+res_id+")'><td>"+res_id+"</td><td>"+im_name+"</td><td id='"+res_id+"_status' >false</td></tr>")
 }
 
+
+function get_dataset_stats(){
+    // alert("yee")
+    fetch("http://203.159.29.187:8080/api/dataset/55/stats", {
+        "headers": {
+          "accept": "application/json",
+          "accept-language": "en-US,en;q=0.9"
+        },
+        "referrer": "http://203.159.29.187:8080/api/",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    })
+    .then(response => response.json())
+    .then(data => stats = data)
+    .then(() => {
+        // alert("yeeee")
+        // if(stats.total){
+            document.getElementById("stats").innerHTML="<p>Total: "+String(stats.total.Images)+", Annotated Images: "+String(stats.total["Annotated Images"])
+            // document.getElementById("stats").innerHTML="<p>"+String(stats.total)+"</p>"
+        // }
+        })
+    .catch(error => console.log(error))
+
+}
+
+
 function upload_to_coco_backend(){
     var uploaded_images = document.getElementById("uploadFile");
       for (var i = 0; i < uploaded_images.files.length; i++) {
@@ -397,9 +626,12 @@ function upload_to_coco_backend(){
         // .then($('<tr id='+new_image_id+' onclick="open_image("'+new_image_id+'")"><td>'+new_image_id+'</td><td>'+uploaded_images.files[i].name+'</td><td id='+new_image_id+'"_status" >false</td></tr>').insertBefore('table > tbody > tr:first'))
         .catch(error => console.log(error))
       }
+    get_dataset_stats()
 }
 
+
 $(document).ready(function(){
+    get_dataset_stats()
     user_from_coco()
     if(set_user){
         get_images_from_coco()
