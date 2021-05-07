9d8f866ac5623a685febe723b12a7ffcfe9777ed
sriramreddyM <srirammandhati@gmail.com>
2021-05-07 13:03:03 +0700

    annotorius

--- docs/js/upload.js
+++ docs/js/upload.js
@@ -1,4 +1,4 @@
-const base_link = "http://cc5c6ee1eee3.ngrok.io"
+const base_link = "https://cc5c6ee1eee3.ngrok.io"
 // const base_link = "http://203.159.29.187:8080"
 // const base_link = "http://203.159.29.51"
 
