57e791fc2366066c6bc6b37495468374d52442bd
sriramreddyM <srirammandhati@gmail.com>
2021-08-19 17:33:58 +0700

    temp baseurl

--- docs/js/map.js
+++ docs/js/map.js
@@ -86,7 +86,8 @@ function addImageToMap(map, xCoordinate, yCoordinate, imageId, prevId, nextId) {
   MARKERS_BY_ID[imageId] = marker;
 
   function imageIdToUrl(id) {
-    const BASE_URL = "https://annotator.ait.ac.th/api/image/";
+    // const BASE_URL = "https://annotator.ait.ac.th/api/image/";
+    const BASE_URL = "http://203.159.29.187:5000/api/image/";
     return BASE_URL + id;
   }
 }
@@ -113,7 +114,8 @@ async function getImagesAndClusters(bounds) {
   var bottom = bounds._southWest.lat;
 
 
-  let response = await fetch("https://annotator.ait.ac.th/api/image/map", {
+  // let response = await fetch("https://annotator.ait.ac.th/api/image/map", {
+  let response = await fetch("http://203.159.29.187:5000/api/image/map", {
     "headers": {
       "accept": "application/json",
       "accept-language": "en-US,en;q=0.9",
