68ec2231e7efeca76b58cd38f09fa3cddec872d0
sriramreddyM <srirammandhati@gmail.com>
2022-01-05 23:24:53 +0700

    load from json

--- plitterstreet/plitter_street.py
+++ plitterstreet/plitter_street.py
@@ -52,16 +52,46 @@ def convert2decimal(coords, ref):
         return decimal_degrees
 
 class StreetData:
-    def __init__(self, path, images_list=''):
+    def __init__(self, path, images_list='', images_json=''):
         self.images_dir = path
         if images_list == '':
             self.images_list = os.path.basename(path)+'.csv'
         else:
             self.images_list = images_list
+        if not os.path.isfile(os.path.join(self.images_dir, self.images_list)):
+            print('Images list file is invalid. Ignore it if the images file names are in order of GPS track.')
+        if images_json == '':
+            self.images_json = os.path.basename(path)+'.json'
+        else:
+            self.images_json = images_list
+        if not os.path.isfile(os.path.join(self.images_dir, self.images_json)):
+            print('Predictions file is invalid. Ignore it if the images are not predicted and saved.')
         self.images = list()
         self.predictions = list()
 
-    def load_images_from_path(self):
+    def load_images_from_dir(self):
+        # a.sort(key=lambda f: int("".join(filter(str.isdigit, f))))
+        self.images = []
+        for filename in os.listdir(self.images_dir).sort(key=lambda f: int("".join(filter(str.isdigit, f)))):
+                img = cv2.imread(os.path.join(self.images_dir,filename))
+                if img is not None:
+                    img_exif = Image(open(os.path.join(self.images_dir,filename), 'rb'))
+                    if img_exif.has_exif:
+                        if 'gps_longitude' in img_exif.list_all() and 'gps_latitude' in img_exif.list_all():
+                            image_dict = dict()
+                            image_dict['name'] = filename
+                            try:
+                                image_dict['latitude'] = convert2decimal(img_exif.gps_latitude, img_exif.gps_latitude_ref)
+                                image_dict['longitude'] = convert2decimal(img_exif.gps_longitude, img_exif.gps_longitude_ref)
+                                self.images.append(image_dict)
+                            except AttributeError:
+                                print('GPS Coordinates not found in', filename)
+                    else:
+                        print('No EXIF information in', filename)
+        print(len(self.images), 'images are found in the directory.')
+
+    def load_images_from_list(self):
+        self.images = []
         if os.path.isfile(os.path.join(self.images_dir, self.images_list)):
             with open(os.path.join(self.images_dir, self.images_list), 'r') as csvf:
                 reader = csv.DictReader(csvf)
@@ -104,7 +134,6 @@ class StreetData:
                         print('No EXIF information in', filename)
             print(len(missing_images), 'images have georefernce and are missing in the list.')
 
-
     def predict_on_images(self, predictor, min_score_thr=0.5, dst_path=''):
         dst_path = self.images_dir
         if not os.path.isdir(os.path.join(dst_path, 'predictions')):
@@ -128,6 +157,12 @@ class StreetData:
                     item = {'image': image['name'], 'class': classes[i].decode(), 'box': [round(float(boxes[i].tolist()[1]), 2), round(float(boxes[i].tolist()[0]), 2), round(float(boxes[i].tolist()[3]), 2), round(float(boxes[i].tolist()[2]), 2)], 'score': round(float(scores[i]), 2)}
                     self.predictions.append(item)
     
+    def load_data_from_json(self):
+        with open(self.images_json) as jsf:
+            data = json.load(jsf)
+            self.images = data['images']
+            self.predictions = data['predictions']
+    
     def save(self, dst_path=''):
         folder_name = os.path.basename(self.images_dir)
         data = dict()
@@ -139,22 +174,22 @@ class StreetData:
 
     def draw_street_on_map(self, m):
         points = self.images
-        m = folium.Map(tiles="OpenStreetMap", prefer_canvas=True)
+        if not type(m) == folium.folium.Map:
+            m = folium.Map(tiles="OpenStreetMap", prefer_canvas=True)
 
-        images = folium.FeatureGroup(name='Images',overlay=True)
-        hotline = folium.FeatureGroup(name='Hot line',overlay=True)
-        heatmap = folium.FeatureGroup(name='Heat map',overlay=True)
-        piles = folium.FeatureGroup(name='Piles',overlay=True)
-        trashbins = folium.FeatureGroup(name='Trash bins',overlay=True)
+            images = folium.FeatureGroup(name='Images',overlay=True)
+            hotline = folium.FeatureGroup(name='Hot line',overlay=True)
+            heatmap = folium.FeatureGroup(name='Heat map',overlay=True)
+            piles = folium.FeatureGroup(name='Piles',overlay=True)
+            trashbins = folium.FeatureGroup(name='Trash bins',overlay=True)
 
+            images.add_to(m)
+            hotline.add_to(m)
+            heatmap.add_to(m)
+            piles.add_to(m)
+            trashbins.add_to(m)
 
-        images.add_to(m)
-        hotline.add_to(m)
-        heatmap.add_to(m)
-        piles.add_to(m)
-        trashbins.add_to(m)
-
-        folium.LayerControl().add_to(m) 
+            folium.LayerControl().add_to(m) 
 
         heat_data = []
         max_count = 0
