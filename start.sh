ca32fb6284b20607e44cdc05fdd5215cd6b5c81a
sriramreddyM <srirammandhati@gmail.com>
2022-11-04 13:02:13 +0700

    clean

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ BASEDIR=$(dirname $0)
 echo "Script location: ${BASEDIR}"
 cd ${BASEDIR}
 
+sleep 100
 git submodule update --init --recursive
 git submodule update --recursive --remote
 #git pull
@@ -14,6 +15,6 @@ echo "pull done"
 /usr/bin/python3 cctv/getWeights.py &
 #/usr/bin/python3 cctv/testWeights.py
 /usr/bin/python3 cctv/upload.py >> /home/cctv/ulog.txt 2>&1 &
-sleep 60
+
 OPENBLAS_CORETYPE=ARMV8 MPLBACKEND=agg /usr/bin/python3 cctv/camera.py >> /home/cctv/clog.txt 2>&1 & /usr/bin/python3 serve/main.py >> /home/cctv/slog.txt 2>&1 &
 /usr/bin/python3 cctv/clean.py >> /home/cctv/cllog.txt 2>&1 &
