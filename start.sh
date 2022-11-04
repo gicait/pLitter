9506062f1a7add293020a129a033f7bb4c76d7fe
sriramreddyM <srirammandhati@gmail.com>
2022-11-04 11:55:25 +0700

    sleep

--- start.sh
+++ start.sh
@@ -10,8 +10,9 @@ git submodule update --recursive --remote
 #git pull
 echo "pull done"
 
-/usr/bin/python3 cctv/getConf.py
-/usr/bin/python3 cctv/getWeights.py
+/usr/bin/python3 cctv/getConf.py & 
+/usr/bin/python3 cctv/getWeights.py &
 #/usr/bin/python3 cctv/testWeights.py
 /usr/bin/python3 cctv/upload.py >> /home/cctv/ulog.txt 2>&1 &
+sleep 60
 OPENBLAS_CORETYPE=ARMV8 MPLBACKEND=agg /usr/bin/python3 cctv/camera.py >> /home/cctv/clog.txt 2>&1 & /usr/bin/python3 serve/main.py >> /home/cctv/slog.txt 2>&1 &
