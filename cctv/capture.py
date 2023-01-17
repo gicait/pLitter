1974e9a07daaaf4a5f043c250ba5a3e3bf3b57de
sriramreddyM <srirammandhati@gmail.com>
2023-01-17 13:36:09 +0700

    save to json and uplo\ad

--- cctv/capture.py
+++ cctv/capture.py
@@ -9,48 +9,80 @@ import numpy as np
 import cv2
 import json
 import torch
-import torchvision
-
+# import torchvision
 import sqlite3
+import uuid
 
-root_dir = os.getenv('root_dir', '/home/cctv/plitter/') #set env var with desired path
-weights = os.getenv('weights', root_dir+'/models/yolov5s.pt')
-FRAME_WIDTH = os.getenv('frame_width', 1920)
-FRAME_HEIGHT = os.getenv('frame_height', 1280)
-interval = int(os.getenv('interval', 60))
+def get_slice_bboxes(image_height,image_width,slice_height,slice_width,overlap_height_ratio,overlap_width_ratio):
+    slice_bboxes = []
+    y_max = y_min = 0
+    y_overlap = int(overlap_height_ratio * slice_height)
+    x_overlap = int(overlap_width_ratio * slice_width)
+    while y_max < image_height:
+        x_min = x_max = 0
+        y_max = y_min + slice_height
+        while x_max < image_width:
+            x_max = x_min + slice_width
+            if y_max > image_height or x_max > image_width:
+                xmax = min(image_width, x_max)
+                ymax = min(image_height, y_max)
+                xmin = max(0, xmax - slice_width)
+                ymin = max(0, ymax - slice_height)
+                slice_bboxes.append([xmin, ymin, xmax, ymax])
+            else:
+                slice_bboxes.append([x_min, y_min, x_max, y_max])
+            x_min = x_max - x_overlap
+        y_min = y_max - y_overlap
+    return slice_bboxes
+
+colors = [(0,255,255), (0,0,255), (255,0,0), (0,255,0)]*20
+
+root_dir = os.getenv('root_dir', '/'.join(os.path.abspath(__file__).split('/')[0:-2]))
+
+yolo_weights = Path(root_dir) / 'models' / os.getenv('weights', 'pLitterFloat_800x752_to_640x640.pt')
+
+FRAME_WIDTH = int(os.getenv('frame_width', 1920))
+FRAME_HEIGHT = int(os.getenv('frame_height', 1080))
+interval = int(os.getenv('interval', 10))
 work_in_night = os.getenv('work_in_night', True)
+weights_url = os.getenv('weights_url', None)
 
-print(root_dir, weights, FRAME_WIDTH, FRAME_HEIGHT, interval, work_in_night)
+print(root_dir, yolo_weights, FRAME_WIDTH, FRAME_HEIGHT, interval, work_in_night)
 
-db_dir = os.path.join(root_dir, 'db')
+if os.path.join(root_dir, 'Yolov5_StrongSORT_OSNet') not in sys.path:
+    print(os.path.join(root_dir, 'Yolov5_StrongSORT_OSNet'))
+    sys.path.append(os.path.join(root_dir, 'Yolov5_StrongSORT_OSNet'))
+if os.path.join(root_dir, 'Yolov5_StrongSORT_OSNet/yolov5') not in sys.path:
+    print(os.path.join(root_dir, 'Yolov5_StrongSORT_OSNet/yolov5'))
+    sys.path.append(os.path.join(root_dir, 'Yolov5_StrongSORT_OSNet/yolov5'))
+
+from yolov5.models.common import DetectMultiBackend
+from yolov5.utils.general import (LOGGER, check_img_size, non_max_suppression, scale_boxes, check_requirements, cv2,
+                                  check_imshow, xyxy2xywh, increment_path, strip_optimizer, colorstr, print_args, check_file)
+
 data_dir = os.path.join(root_dir, 'data')
-os.makedirs(db_dir, exist_ok=True)
-os.makedirs(data_dir, exist_ok=True)
-
-#print(root_dir, db_dir, data_dir)
-
-detections_dbpath = os.path.join(db_dir, 'detections.db') # change this
-images_dbpath = os.path.join(db_dir, 'images.db')
-
-print(detections_dbpath)
-conn = sqlite3.connect(detections_dbpath, isolation_level=None)
-cur = conn.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS detections(id INTEGER PRIMARY KEY, track_id INTEGER, date_time TEXT, cat_id INTEGER, bbox TEXT, segmentation TEXT)""")
-conn.commit()
-
-im_conn = sqlite3.connect(images_dbpath, isolation_level=None)
-im_cur = im_conn.cursor()
-im_cur.execute("""CREATE TABLE IF NOT EXISTS images(id INTEGER PRIMARY KEY, file_name TEXT UNIQUE, uploaded BOOLEAN)""")
-im_conn.commit()
 
 # torch.set_printoptions(precision=3)
 
-slice_boxes = [[0, 0, 720, 840], [648, 0, 1368, 840], [1200, 0, 1920, 840], [0, 440, 720, 1280], [648, 440, 1368, 1280], [1200, 440, 1920, 1280]]
+silce_width = int(os.getenv("silce_width", 800))
+slice_height = int(os.getenv("slice_height", 752))
+
+slice_boxes = get_slice_bboxes(FRAME_HEIGHT, FRAME_WIDTH, slice_height, silce_width, 0.04, 0.04)
 
 device = torch.device('cuda:0')
 half = True
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', weights)
+#model = torch.hub.load('ultralytics/yolov5', 'custom', weights, skip_validation=True)
+if not os.path.isfile(yolo_weights):
+    #download weights here
+    try:
+        torch.hub.download_url_to_file(weights_url, yolo_weights)
+    except:
+        yolo_weights = Path(root_dir) / 'models/yolov5s.pt'
+        pass
+
+model = DetectMultiBackend(yolo_weights, device=device, fp16=half)
+stride, names, pt = model.stride, model.names, model.pt
 print(model.names)
 
 t0 = time.time()
@@ -59,14 +91,17 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(FRAME_WIDTH))
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(FRAME_HEIGHT))
 
-cur = conn.cursor()
-im_cur = im_conn.cursor()
+imgsz = 640
+
+prev_frame, curr_frame = None, None
+
+start = '06:00:00'
+end = '18:00:00'
+timer = time.time()
 
 with torch.no_grad():
     while True:
         current_time = datetime.now().strftime("%H:%M:%S")
-        start = '06:00:00'
-        end = '18:00:00'
         if current_time >= end or current_time < start:
             if work_in_night in (False, 'False'):
                 print('night mode turning off')
@@ -76,30 +111,58 @@ with torch.no_grad():
         st = time.time()
         im_name = datetime.now().strftime("%Y%m%d_%H%M%S")
         
+        pred_json = {'image': im_name+'.jpg', 'preds': []}
+
         ret, img0 = cap.read()
+        #imgr = img0
         #img0 = cv2.imread(root_dir+'/cctv/test.jpg') #image for test
-        
+
+        if img0 is None:
+            print("check if camera is connected and run again \n")
+            continue
+
         if img0.all() is None:
             continue
+        
         preds = torch.tensor([], dtype=torch.float16)
+        
         for box in slice_boxes:
             img = img0[box[1]:box[3], box[0]:box[2], :]
+            h,w,_ = img.shape
+            h_r = h/imgsz
+            w_r = w/imgsz
+            img = cv2.resize(img, (imgsz, imgsz), interpolation=cv2.INTER_LINEAR)
+            img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+            img = np.ascontiguousarray(img)
+            img = torch.from_numpy(img).to(device)
+            img = img.half() if half else img.float() #unint8 to fp16 or fp32
+            img /= 255.0 #normalize to between 0 and 1.
+            if img.ndimension()==3:
+                img = img.unsqueeze(0)
+            
             pred = model(img)
-            proc_pred = pred.xyxy[0].cpu()
+            pred = non_max_suppression(pred, 0.4, 0.5)
+
+            #print(pred)
+            proc_pred = pred[0].cpu()
+            
             for i, det in enumerate(proc_pred):
-                proc_pred[i][0] += box[0]
-                proc_pred[i][1] += box[1]
-                proc_pred[i][2] += box[0]
-                proc_pred[i][3] += box[1]
+                proc_pred[i][0] = proc_pred[i][0]*w_r + box[0]
+                proc_pred[i][1] = proc_pred[i][1]*h_r + box[1]
+                proc_pred[i][2] = proc_pred[i][2]*w_r + box[0]
+                proc_pred[i][3] = proc_pred[i][3]*h_r + box[1]
             preds = torch.cat((preds, proc_pred), 0)
+        
+        #print("preds:", preds)
+        
         for pred in preds:
             pred = pred.tolist()
             bbox = [pred[0], pred[1], pred[2]-pred[0], pred[3]-pred[1]]
-            segmentation = [[pred[0], pred[1], pred[2], pred[1], pred[2], pred[3], pred[0], pred[3]]]
-            cur.execute("""INSERT INTO detections (track_id, date_time, cat_id, bbox, segmentation) values(?,?,?,?,?)""", ( 0, im_name, int(pred[4]), json.dumps(bbox), json.dumps(segmentation)))
+            seg = [[pred[0], pred[1], pred[2], pred[1], pred[2], pred[3], pred[0], pred[3]]]
+            pred_json['preds'].append({'category': model.names[int(pred[5])], 'bbox': bbox, 'segmentation': seg})
+        
+        print(pred_json)
         im_save = cv2.imwrite(data_dir+'/'+im_name+'.jpg', img0)
-        if im_save:
-            im_cur.execute("""INSERT INTO images (file_name, uploaded) values(?,?)""", (im_name, False))
-        fn = time.time()
-        print("time:", fn-st)
+        print(im_save)
+        json.dump(pred_json, open(data_dir+'/'+im_name+'.json', 'w'))
         time.sleep(interval)
---
+++ cctv/upload_files.py
@@ -0,0 +1,79 @@
+import os
+import json
+import requests
+import time
+import cv2
+import json
+
+class_map = {'debris': 'plastic', 'bottle': 'Plastic bottle', 'styrofoam': 'Styrofoam', 'trash bag': 'Trash bag', 'straw': 'Straw'}
+
+# ------make sure below env vars are set before run----------
+root_dir = os.getenv('root_dir', 'project_home')
+url = os.getenv('url', None)
+dataset_id = os.getenv('dataset_id', None)
+key = os.getenv('key', '--secrte-key--')
+
+print(root_dir, url, dataset_id)
+
+if not (url and dataset_id):
+    print("need api details to uplaod")
+    exit()
+
+image_url = url+'/api/image/'
+prediction_url = url+'/api/annotator/predictions'
+
+image_dir = root_dir+'/data'
+headers = {"Content-Type": "application/json; charset=utf-8"}
+
+while True:
+    images_list = sorted([im for im in os.listdir(image_dir) if im.endswith('.jpg')])
+    for image in images_list:
+        print(image)
+        if image.endswith(('.png', '.jpg')) and os.path.isfile(os.path.join(image_dir,image)):
+            files = {'image': open(os.path.join(image_dir,image), 'rb')}
+            data = {'dataset_id': dataset_id, 'key': key}
+            try:
+                r = requests.post(image_url, files=files, data=data, timeout=119)
+                print(r.status_code)
+                if r.status_code in (200, 400):
+                    print("image uploaded successfully")
+                    r_json = r.json()
+                    print(r_json)
+                    if 'image_id' in r_json.keys():
+                        image_id = r_json['image_id']
+                        print(image_id)
+                        json_data = json.load(open(os.path.join(image_dir,image.replace('.jpg', '.json')), 'rb'))
+                        if len(json_data['preds']) == 0:
+                            print('no detections')
+                            os.remove(os.path.join(image_dir, image))
+                            os.remove(os.path.join(image_dir, image.replace('.jpg', '.json')))
+                        else:
+                            det_data = {'image_id': image_id, 'key': key, 'detections': []}
+                            for det in  json_data['preds']:
+                                det_data['detections'].append({
+                                    'category': class_map[det['category']] if det['category'] in class_map.keys() else 'plastic',
+                                    'isbbox': True,
+                                    'bbox': det['bbox'],
+                                    'segmentation': det['segmentation']
+                                })
+                            print(det_data)
+                            rr = requests.post(prediction_url, json=det_data, timeout=119)
+                            print(rr.status_code, rr.json())
+                            if rr.status_code == 200:
+                                print('detections uploaded successfully')
+                                os.remove(os.path.join(image_dir, image))
+                                os.remove(os.path.join(image_dir, image.replace('.jpg', '.json')))
+                    else:
+                        pass
+                else:
+                    print("failed to upoad the image")
+            except requests.exceptions.ConnectionError as errc:
+                print("Error connecting:", errc)
+            except requests.exceptions.HTTPError as errh:
+                print("Http error:", errh)
+            except requests.exceptions.Timeout as errt:
+                print("Timeout error:", errt)
+            except requests.exceptions.RequestException as err:
+                print("Uknown:", err)
+    print("sleep for a minute") 
+    time.sleep(60)
